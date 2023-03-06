45692d19eba86898d7822cc24e07d6fb1758b051
LindsayBrumley <lalainaphillips@gmail.com>
2023-03-06 11:12:28 -0500

    Rows

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here
+
+const table = document.getElementsByTagName("table")[0];
+
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table.appendChild(row);
+}
+makeRow();
+makeRow();
+
+const addRow = document.getElementById("add-row");
+addRow.addEventListener("click", makeRow);
